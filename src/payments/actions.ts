ac83450c6d03a8d63e9dd084eab466fdcb564db2
Donald Ngai <donaldptngai@gmail.com>
2025-12-18 17:46:32 -0500

    Added new rent page and fixed crm

--- src/payments/actions.ts
+++ src/payments/actions.ts
@@ -1,16 +1,28 @@
 'use server';
 
 import { redirect } from 'next/navigation';
-import { createCustomerPortalSession } from './stripe';
-import { getTeamForCurrentUser } from './subscription';
+import { createCheckoutSession, createCustomerPortalSession } from './stripe';
+import { getCurrentUserFullDetails } from '../auth/users';
 
-export async function customerPortalAction() {
-  const team = await getTeamForCurrentUser();
-
-  if (!team?.stripeCustomerId) {
+export async function checkoutAction(formData: FormData) {
+  const priceId = formData.get('priceId') as string;
+  
+  if (!priceId) {
     redirect('/pricing');
   }
 
-  const portalSession = await createCustomerPortalSession(team.stripeCustomerId);
-  redirect(portalSession.url);
+  const user = await getCurrentUserFullDetails();
+
+  // await createCheckoutSession({ team, priceId });
+}
+
+export async function customerPortalAction() {
+  // const team = await getTeamForUser();
+
+  // if (!team) {
+  //   redirect('/pricing');
+  // }
+
+  // const portalSession = await createCustomerPortalSession(team);
+  // redirect(portalSession.url);
 }
